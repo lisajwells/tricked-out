9317c41718af13aab46cb484ea294413db1bd6ad
Lisa Wells <lisawells999@gmail.com>
2017-06-20 16:37:01 -0400

    add browser-sync, error-handling

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,35 @@ require('es6-promise').polyfill();
 var gulp          = require('gulp');
 var sass          = require('gulp-sass');
 var autoprefixer  = require('gulp-autoprefixer');
+// keeps gulp from breaking if there's an error
+var plumber = require('gulp-plumber');
+// to better handle errors
+var gutil = require('gulp-util');
+// for live refresh on changes
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var onError = function (err) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
+  .pipe(plumber({ errorHandler: onError }))
   .pipe(sass())
   .pipe(autoprefixer())
   .pipe(gulp.dest('./'))
 });
 
-gulp.task('default', ['sass']);
+gulp.task('watch', function() {
+  browserSync.init({
+    files: ['./**/*.php'],
+    proxy: 'http://tricked-out-images.dev/',
+    // proxy: 'http://localhost:8888/wordpress/',
+  });
+  gulp.watch('./sass/**/*.scss', ['sass', reload]);
+});
+
+gulp.task('default', ['sass', 'watch']);
