049e80f4d78c1b70cb60d29b35c8b2d992bb958f
Lisa Wells <lisawells999@gmail.com>
2017-06-20 16:05:47 -0400

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 require('es6-promise').polyfill();
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
 
-var gulp = require('gulp');
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
+});
 
-// default task
-gulp.task('default');
+gulp.task('default', ['sass']);
