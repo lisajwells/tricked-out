5793547a0f46c5d6a1878d2377d8e9b8c44ebce3
Lisa Wells <lisawells999@gmail.com>
2017-06-21 17:59:32 -0400

    add fonts; start scss for header

--- functions.php
+++ functions.php
@@ -5,6 +5,25 @@
  * @package Generate
  */
 
+function trick_add_google_fonts() {
+
+wp_enqueue_style( 'trick-google-fonts', 'http://fonts.googleapis.com/css?family=Palanquin+Dark:400,600|Lato:400', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'trick_add_google_fonts' );
+
+// add another google font to GeneratePress menu
+add_filter( 'generate_typography_default_fonts','trick_add_system_fonts' );
+function trick_add_system_fonts( $fonts ) {
+    $fonts[] = 'Palanquin Dark';
+    return $fonts;
+}
+
+// font-family: 'Lato', sans-serif;
+// // regular 400
+// font-family: 'Palanquin Dark', sans-serif;
+// // regular 400, semi-bold 600
+
 // customize the customizer palettes
 add_filter( 'generate_default_color_palettes', 'trick_custom_color_palettes' );
 function trick_custom_color_palettes( $palettes ) {
