97da6b737df919f0ea9f10c4f0d0d7bad4d00a3a
Lisa Wells <lisawells999@gmail.com>
2017-06-21 15:07:30 -0400

    add sass mixins

--- functions.php
+++ functions.php
@@ -4,5 +4,30 @@
  *
  * @package Generate
  */
- 
- 
+
+// customize the customizer palettes
+add_filter( 'generate_default_color_palettes', 'trick_custom_color_palettes' );
+function trick_custom_color_palettes( $palettes ) {
+    $palettes = array(
+        '#000000',
+        '#222222',
+        '#FF652F',
+        '#FFE401',
+        '#13A76B',
+        '#747474',
+        '#FF9862',
+        '#FFFFFF',
+    );
+
+    return $palettes;
+}
+
+/*
+ * Add Logo support
+ */
+// add_theme_support( 'custom-logo', array(
+//     'height' => 250,
+//     'width' => 276,
+//     'flex-height' => true,
+//     'flex-width' => true,
+// ) );
